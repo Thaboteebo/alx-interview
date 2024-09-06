d36b552cfbd8fd6e8c09e3af86fe32e85d169dfe
Thaboteebo <Thaboteebo01@gmail.com>
2024-09-06 05:36:08 -0700

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
+
 const request = require('request');
 const movieID = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieID}/`;
@@ -11,18 +12,13 @@ request(url, (error, response, body) => {
 		return;
 	}
 
-
-	const film = JSON.parse(body);
-	const characters = film.characters;
-
-
+	const film = JSON.parse(body);const characters = film.characters;
 	characters.forEach((character) => {
 		request(character, (error, response, body) => {
 			if (error) {
 				console.error('error:', error);
 				return;
 			}
-
 
 			const characterData = JSON.parse(body);
 			console.log(characterData.name);
